4f6885e213d95f7763681eea40be962a193a511c
Lucile Gentner <lucile.gentner@gmail.com>
2018-04-18 17:27:50 +0200

    First - messy - script version

--- console.php
+++ console.php
@@ -14,6 +14,10 @@ Example:
 ------------------------------------------
 */
 
+use Catalog\Entity\XmlPath;
+use Catalog\Entity\Song;
+use Catalog\Entity\Album;
+
 //~ Report & Display all error
 error_reporting(-1);
 ini_set('display_errors', true);
@@ -52,16 +56,91 @@ echo 'Process file: ' . $file . PHP_EOL;
 if (file_exists('xml/825646618309.xml')) {
     $xml = simplexml_load_file('xml/825646618309.xml');
 
-$i = 0;
-    foreach ($xml as $oneline) {
-        print_r($oneline);
-        $i++;
-        if ($i > 1) {
-            die;
+
+    //Get fields to parse order by group paths
+    $qb = $entityManager->createQueryBuilder();
+
+    $qb->select('distinct xp.groupPath')
+        ->from(XmlPath::class, 'xp')
+        ->where('xp.fileName = :fileName')
+        ->orderBy('xp.groupPath')
+        ->groupBy('xp.groupPath')
+        ->setParameter('fileName','825646618309.xml');
+
+    $groupPaths = $qb->getQuery()->getScalarResult();
+
+    $album = new Album();
+    $entityManager->persist($album);
+
+    foreach ($groupPaths as $groupPath) {/*
+        print_r($groupPath['groupPath']);
+        print_r("--------------\n");*/
+
+
+        $qb = $entityManager->createQueryBuilder();
+
+        $qb->select('xp.subPath, xp.xmlFieldName, xp.fieldName, xp.objectType')
+            ->from(XmlPath::class, 'xp')
+            ->where('xp.fileName = :fileName')
+            ->andWhere('xp.groupPath = :groupPath')
+            ->setParameter('fileName','825646618309.xml')
+            ->setParameter('groupPath', $groupPath['groupPath']);
+
+        $fieldPaths = $qb->getQuery()->getResult();
+        $elements = $xml->xpath($groupPath['groupPath']);
+        $position = 1;
+
+        $struct = explode('/',$groupPath['groupPath']);
+        $referenceTag = str_replace('List','Reference', $struct[0]);
+        print_r($referenceTag);
+
+        foreach ($elements as $element) {
+            //Get Reference
+
+            $reference = (int)substr($element->{$referenceTag}, 1);
+
+            $song = $entityManager
+                ->getRepository(Song::class)
+                ->findOneBy(array(
+                    'songNumber'=>$reference,
+                    'album' => $album));
+
+            if (empty($song)) {
+                $song = new Song();
+                $song->setSongNumber($reference);
+                $song->setAlbum($album);
+            }
+
+            foreach($fieldPaths as $path) { //get all data from this group
+
+
+                if (isset($element->xpath($path['subPath'])[0]->{$path['xmlFieldName']})) {
+
+                    //insert in database
+//                    print_r((string)$element->xpath($path['subPath'])[0]->{$path['xmlFieldName']} . "\n");
+//                    print_r("....".$reference . "\n");
+
+                    if ($path['objectType'] === 'Song') {
+                        $song->{"set" . ucfirst($path['fieldName'])}((string)$element->xpath($path['subPath'])[0]->{$path['xmlFieldName']});
+                    } else if ($path['objectType'] === 'Album') {
+                        $album->{"set" . ucfirst($path['fieldName'])}((string)$element->xpath($path['subPath'])[0]->{$path['xmlFieldName']});
+                    }
+
+
+                }
+
+
+            }
+            $entityManager->persist($song);
+            $entityManager->flush();
+
+            unset($song);
         }
-
-
     }
+    $entityManager->persist($album);
+    $entityManager->flush();
+    
+
 } else {
     exit('Echec lors de l\'ouverture du fichier test.xml.');
 }
